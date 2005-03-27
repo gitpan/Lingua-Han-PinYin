f2717366432b24a534e3cc3ac123a97dc2422b60
Fayland 林 <fayland@gmail.com>
2005-03-27 08:28:00 +0000

    Import of FAYLAND/Lingua-Han-PinYin-0.01 from CPAN.
    gitpan-cpan-distribution: Lingua-Han-PinYin
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         FAYLAND/Lingua-Han-PinYin-0.01.tar.gz
    gitpan-cpan-author:       FAYLAND
    gitpan-cpan-maturity:     released

---
+++ lib/Lingua/Han/PinYin.pm
@@ -0,0 +1,85 @@
+package Lingua::Han::PinYin;
+
+use strict;
+use vars qw($VERSION);
+$VERSION = '0.01';
+
+sub new {
+	my $class = shift;
+	my $dir = __FILE__; $dir =~ s/\.pm//o;
+	-d $dir or die "Directory $dir nonexistent!";
+	my $self = { '_dir_' => $dir, @_ };
+	unless ($self->{'format'}) { $self->{'format'} = 'gb2312'; }
+	my %py;
+	my $file = $self->{'_dir_'} . "/PinYin_" . $self->{'format'} . ".txt";
+	open(FH, $file)	or die "$file: $!";
+	while(<FH>) {
+		my ($uni, $py) = split(/\s+/);
+		$py{$uni} = $py;
+	}
+	close(FH);
+	$self->{'py'} = \%py;
+	return bless $self => $class;
+}
+
+sub han2pinyin {
+	my ($self, $hanzi) = @_;
+	my $uni = unpack("H*",$hanzi);
+	if (exists $self->{'py'}->{$uni}) {
+		return $self->{'py'}->{$uni};
+	} else {
+		return 'XX';
+	}
+}
+
+1;
+__END__
+
+=head1 NAME
+
+Lingua::Han::PinYin - Retrieve the PinYin of Chinese character.
+
+=head1 SYNOPSIS
+
+  use Lingua::Han::PinYin;
+  
+  # if the format of your script is gb2312, default
+  my $h2p = new Lingua::Han::PinYin();
+  print $h2p->han2pinyin("我");
+  
+  # if the format of your script is utf-8
+  my $h2p = new Lingua::Han::PinYin(format => 'utf8');
+  print $h2p->han2pinyin("我");
+
+=head1 DESCRIPTION
+
+There is a Chinese document @ L<http://www.fayland.org/journal/Han2PinYin.html>. It tells why and how I write this module.
+
+=head1 RESTRICTIONS
+
+if the character is polyphone(DuoYinZi), we can B<NOT> point out the correct one.
+
+=head1 OPTION
+
+=head1 RETURN VALUE
+
+if it's a common character, it returns its pinyin/spell.
+
+if not, it returns 'XX';
+
+=head1 SEE ALSO
+
+L<Unicode::Unihan>
+
+=head1 AUTHOR
+
+Fayland, fayland@gmail.com
+
+=head1 COPYRIGHT
+
+Copyright (c) 2005 Fayland All rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+See L<http://www.perl.com/perl/misc/Artistic.html>
---
+++ t/2-utf8.t
@@ -0,0 +1,19 @@
+# Before `make install' is performed this script should be runnable with
+# `make test'. After `make install' it should work as `perl Lingua-Han2PinYin.t'
+
+#########################
+
+# change 'tests => 1' to 'tests => last_test_to_print';
+
+use Test::More tests => 3;
+BEGIN { use_ok('Lingua::Han::PinYin') };
+
+#########################
+
+# Insert your test code below, the Test::More module is use()ed here so read
+# its man page ( perldoc Test::More ) for help writing this test script.
+
+my $h2p = new Lingua::Han::PinYin(format => 'utf8');
+is(ref($h2p) => 'Lingua::Han::PinYin', 'class');
+my $pinyin = $h2p->han2pinyin("我");
+is($pinyin, 'wo', 'correct');
